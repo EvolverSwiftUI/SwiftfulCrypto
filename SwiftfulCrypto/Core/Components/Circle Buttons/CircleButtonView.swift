1a55cba127248b8707fbc5c4e97c40bed226ffcb
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-24 01:21:16 +0530

    Added the coin model and its basic values setup done

---
+++ SwiftfulCrypto/Core/Components/Circle Buttons/CircleButtonView.swift
@@ -0,0 +1,44 @@
+//
+//  CirclebuttonView.swift
+//  SwiftfulCrypto
+//
+//  Created by Sivaram Yadav on 11/23/21.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    
+    let iconName: String
+    
+    var body: some View {
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundColor(Color.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+                Circle()
+                    .foregroundColor(Color.theme.background)
+            )
+            .shadow(
+                color: Color.theme.accent.opacity(0.25),
+                radius: 10,
+                x: 0, y: 0
+            )
+            .padding()
+    }
+}
+
+struct CirclebuttonView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CircleButtonView(iconName: "info")
+                .preferredColorScheme(.light)
+                .previewLayout(.sizeThatFits)
+
+            CircleButtonView(iconName: "plus")
+                .colorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
