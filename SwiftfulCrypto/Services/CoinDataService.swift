6303d971838a1e6d885d159169aba1111c477485
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-25 21:09:21 +0530

    Added a Network component

---
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -0,0 +1,38 @@
+//
+//  CoinDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by Sivaram Yadav on 11/25/21.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+        
+    // but problem , difficult to figure out the correct one to cancel.
+    // var cancallables = Set<AnyCancellable>()
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+         
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+        )
+        else {
+            return
+        }
+        
+        coinSubscription = NetworkManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
