79df1652ae6ec0d7a19b10cb76f76d9d46eb71b3
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-26 00:13:21 +0530

    Added code to save and retrieve images from local cache directory by using file manager concept

--- SwiftfulCrypto/Services/CoinImageService.swift
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -12,15 +12,30 @@ import Combine
 class CoinImageService {
     
     @Published var image: UIImage?
-    var imageSubscription: AnyCancellable?
+    private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private var imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from File Manager!")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now!")
+        }
+    }
+    
+    private func downloadCoinImage() {
         
         guard let url = URL(string: coin.image) else { return }
         
@@ -29,8 +44,11 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self,
+                      let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
