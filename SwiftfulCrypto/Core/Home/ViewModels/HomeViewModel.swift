404fd414b87b03bcf41c2d46c03c4d1ffb2650ae
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-26 13:19:31 +0530

    Added filter functionality for typed text on search bar

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -30,14 +30,55 @@ class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
+        
+        // as below subscriber will work like same way as:
+        // to update allcoins array
+        // so put comment this code, no need to write twice here
+        /*
         dataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        */
+        
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink(receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            })
+            .store(in: &cancellables)
     }
     
-    
+    func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        // make code little cleaner like below:
+        /*
+        let lowercasedText = text.lowercased()
+
+        let filteredCoins = startingCoins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.lowercased().contains(lowercasedText) ||
+                coin.id.lowercased().contains(lowercasedText)
+        }
+
+        return filteredCoins
+         */
+        
+        let lowercasedText = text.lowercased()
+
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.lowercased().contains(lowercasedText) ||
+                coin.id.lowercased().contains(lowercasedText)
+        }
+    }
     
     
     
