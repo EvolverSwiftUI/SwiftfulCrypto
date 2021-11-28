1d5b6574b92708dea5e137c64fc7f78fa4777776
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-28 22:29:25 +0530

    Added a custom navigation link and also added a lazy loading detail view

---
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,44 @@
+//
+//  DetailView.swift
+//  SwiftfulCrypto
+//
+//  Created by Sivaram Yadav on 11/28/21.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    
+    @Binding var coin: CoinModel?
+        
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Initializing Detail View for \(coin.name)")
+    }
+    
+    var body: some View {
+        ZStack {
+            Text(coin.name)
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(coin: dev.coin)
+    }
+}
