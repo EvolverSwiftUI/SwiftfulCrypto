4890b2442286d25153d18fe7e88d7e9d8ee6fdd2
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2021-11-28 18:18:30 +0530

    Added sort functionality for all columns

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -25,11 +25,16 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     @Published var isLoading: Bool = false
     @Published var searchText: String = ""
+    @Published var sortOption: SortOption = .holdings
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
     private let portfolioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable>()
+    
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price, priceReversed
+    }
     
     init() {
         
@@ -56,9 +61,9 @@ class HomeViewModel: ObservableObject {
         
         // updates allCoins
         $searchText
-            .combineLatest(coinDataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins, $sortOption)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .map(filterCoins)
+            .map(filterAndSortCoins)
             .sink(receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             })
@@ -69,7 +74,8 @@ class HomeViewModel: ObservableObject {
             .combineLatest(portfolioDataService.$savedEntities)
             .map(mapAllCoinsToPortfolioCoins)
             .sink { [weak self] (returnedCoins) in
-                self?.portfolioCoins = returnedCoins
+                guard let self = self else { return }
+                self.portfolioCoins = self.sortPortfolioCoinsIfNeeded(coins: returnedCoins)
             }
             .store(in: &cancellables)
 
@@ -96,6 +102,20 @@ class HomeViewModel: ObservableObject {
         HapticManager.notification(type: .success)
     }
     
+    private func filterAndSortCoins(text: String, coins: [CoinModel], sort: SortOption) -> [CoinModel] {
+        
+        // sort - way 1
+        //var filteredCoins = filterCoins(text: text, coins: coins)
+        // let sortedCoins = sortCoins(sort: sort, coins: filteredCoins)
+        
+        // sort - way 2 -
+        // use same array of coins and just change places insread of create a new array
+        // meaning it won't creates a new array like above
+        var updatedCoins = filterCoins(text: text, coins: coins)
+        sortCoins(sort: sort, coins: &updatedCoins)
+        return updatedCoins
+    }
+    
     func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
         guard !text.isEmpty else {
             return coins
@@ -118,13 +138,55 @@ class HomeViewModel: ObservableObject {
 
         return coins.filter { (coin) -> Bool in
             return coin.name.lowercased().contains(lowercasedText) ||
-                coin.symbol.lowercased().contains(lowercasedText) ||
-                coin.id.lowercased().contains(lowercasedText)
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
         }
     }
     
-    func mapAllCoinsToPortfolioCoins(allCoins: [CoinModel],
-                                     portfolioEntities: [PortfolioEntity]) -> [CoinModel] {
+    // way 1 - sort - which creates new array and then it will returns
+    func sortCoins(sort: SortOption, coins: [CoinModel]) -> [CoinModel] {
+        
+        switch sort {
+        case .rank, .holdings:
+            return coins.sorted(by: { $0.rank < $1.rank })
+        case .rankReversed, .holdingsReversed:
+            return coins.sorted(by: { $0.rank > $1.rank })
+        case .price:
+            return coins.sorted(by: { $0.currentPrice < $1.currentPrice })
+        case .priceReversed:
+            return coins.sorted(by: { $0.currentPrice > $1.currentPrice })
+        }
+    }
+    
+    func sortPortfolioCoinsIfNeeded(coins: [CoinModel]) -> [CoinModel] {
+        // will only sort by holdings or reversedholdings if needed
+        switch sortOption {
+        case .holdings:
+            return coins.sorted(by: { $0.currentHoldingsValue > $1.currentHoldingsValue })
+        case .holdingsReversed:
+            return coins.sorted(by: { $0.currentHoldingsValue < $1.currentHoldingsValue })
+        default:
+            return coins
+        }
+    }
+    
+    // way 2 - sort - which uses same array and just changes the places of same array
+    // ans it won't create new array
+    func sortCoins(sort: SortOption, coins: inout [CoinModel]) {
+        
+        switch sort {
+        case .rank, .holdings:
+            coins.sort(by: { $0.rank < $1.rank })
+        case .rankReversed, .holdingsReversed:
+            coins.sort(by: { $0.rank > $1.rank })
+        case .price:
+            coins.sort(by: { $0.currentPrice < $1.currentPrice })
+        case .priceReversed:
+            coins.sort(by: { $0.currentPrice > $1.currentPrice })
+        }
+    }
+
+    func mapAllCoinsToPortfolioCoins(allCoins: [CoinModel], portfolioEntities: [PortfolioEntity]) -> [CoinModel] {
         allCoins
             .compactMap { (coin) -> CoinModel? in
                 guard let entity = portfolioEntities.first(where: { $0.coinID == coin.id }) else {
@@ -178,14 +240,4 @@ class HomeViewModel: ObservableObject {
         
         return stats
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
